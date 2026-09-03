71ca5340aafdb34dd2dfce81e21cd89faa9256cf
Hgangajadiya <hgangajadiya@gmail.com>
2026-09-03 10:42:38 +0530

    Format and clean up app.js code style

--- app.js
+++ app.js
@@ -5,52 +5,49 @@ const btn=document.querySelector("#suggest-btn");
 const diff=document.querySelector('select[name="difficulty"]');
 const suggbox=document.querySelector(".suggestion-box");
 
-for(let i in topics)
-{
-    let topic=document.createElement("option");
-    topic.innerText=i;
-    topic.value=i;
+for (let i in topics) {
+    let topic = document.createElement("option");
+    topic.innerText = i;
+    topic.value = i;
     topicSelect.append(topic);
-    
+
 }
-topicSelect.addEventListener("change",()=>{
+topicSelect.addEventListener("change", () => {
     subtopicSelect.innerHTML = `<option value="" disabled selected hidden>--select--</option>`;
-    for(let j of topics[topicSelect.value])
-    {
-        let subtopic=document.createElement("option");
-        subtopic.innerText=j;
-        subtopic.value=j;
+    for (let j of topics[topicSelect.value]) {
+        let subtopic = document.createElement("option");
+        subtopic.innerText = j;
+        subtopic.value = j;
         subtopicSelect.append(subtopic);
     }
 });
-btn.addEventListener("click",async()=>{
+btn.addEventListener("click", async () => {
     let data;
-    try{
-        let response=await fetch(api);
-        data=await response.json();
+    try {
+        let response = await fetch(api);
+        data = await response.json();
     }
-    catch{
-        suggbox.innerHTML="API ma vandho che bhai";
+    catch {
+        suggbox.innerHTML = "API ma vandho che bhai";
     }
-    let filtered=data.filter(problem =>
+    let filtered = data.filter(problem =>
         (diff.value === "" || problem.difficulty === diff.value)
-        && (topicSelect.value==="" || problem.topic_tags.includes(topicSelect.value))
-        && (subtopicSelect.value==="" || problem.topic_tags.includes(subtopicSelect.value))
+        && (topicSelect.value === "" || problem.topic_tags.includes(topicSelect.value))
+        && (subtopicSelect.value === "" || problem.topic_tags.includes(subtopicSelect.value))
     );
-    if(filtered.length === 0){
-        suggbox.innerHTML="Not Exist";
+    if (filtered.length === 0) {
+        suggbox.innerHTML = "Not Exist";
         return;
     }
-    suggbox.innerHTML="";
-    let cnt=0;
-    for(let que of filtered)
-    {
-        if(cnt===50)    break;
-        let link=document.createElement("a");
-        link.innerText=`${que.id}. ${que.title}`;
-        link.href=que.url;
-        link.target="_blank";
+    suggbox.innerHTML = "";
+    let cnt = 0;
+    for (let que of filtered) {
+        if (cnt === 50) break;
+        let link = document.createElement("a");
+        link.innerText = `${que.id}. ${que.title}`;
+        link.href = que.url;
+        link.target = "_blank";
         cnt++;
         suggbox.append(link);
-    } 
+    }
 });
